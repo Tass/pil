010aff9725e8d36c487def9fcb71364e05c1cd91
Chris Cummer <chris@postal-code.com>
2012-12-23 09:18:22 -0800

    Moved Pil into its own lib file

--- lib/pil.rb
+++ lib/pil.rb
@@ -1,25 +1,3 @@
 require 'forwardable'
+require 'pil/pil'
 require 'pil/password_list'
-
-class Pil
-  extend Forwardable
-
-  def initialize
-    @passwords = PasswordList.new
-  end
-
-  # ------------------------------ Class Methods ------------------------------
-
-  def self.include?(password)
-    Pil.new.include?(password)
-  end
-
-  def self.exclude?(password)
-    !self.include?(password)
-  end
-
-  # ------------------------------ Instance Methods ------------------------------
-
-  def_delegator :@passwords, :include?, :include?
-  def_delegator :@passwords, :exclude?, :exclude?
-end
